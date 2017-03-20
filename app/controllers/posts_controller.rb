f4e4534eefb64d65a888fc36f8328e77eba0b605
olegsugatov <oleg.sugatov@gmail.com>
2017-03-20 18:56:15 +0300

    fully blog functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
+  
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
+  	@posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -19,7 +22,29 @@ class PostsController < ApplicationController
   	@post = Post.find(params[:id])
   end
 
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to root_path
+  end
+
   private
+
+  def find_post
+  	@post = Post.find(params[:id])
+  end
 
   def post_params
   	params.require(:post).permit(:title,:body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  root 'posts#index'
   # get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
